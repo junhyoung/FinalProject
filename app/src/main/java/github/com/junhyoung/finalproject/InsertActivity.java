80429a91f28e09dd465171cfb2c2a3fa43761315
junhyeong <zz7193@naver.com>
2015-12-16 12:08:44 +0900

    activity + gps입력(12.16) + 날짜(12.16)

--- app/src/main/java/github/com/junhyoung/finalproject/InsertActivity.java
+++ app/src/main/java/github/com/junhyoung/finalproject/InsertActivity.java
@@ -1,6 +1,8 @@
 package github.com.junhyoung.finalproject;
 
 import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -9,7 +11,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.text.NumberFormat;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
 
 public class InsertActivity extends AppCompatActivity {
 
@@ -18,11 +24,52 @@ public class InsertActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
-        Log.d("Main", "onCreate");
 
         logView = (TextView) findViewById(R.id.log);
         logView.setText("GPS 가 잡혀야 좌표가 구해짐");
+        findlocate();
+        findDay();
 
+
+    }
+
+    public void findDay() {
+        TextView day = (TextView) findViewById(R.id.day);
+        day.setText(+ Calendar.getInstance().get(Calendar.YEAR) +"." + (Calendar.getInstance().get(Calendar.MONTH) + 1) +"." + Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+
+    }
+
+    private String findAddress(double lat, double lng) {
+        StringBuffer bf = new StringBuffer();
+        Geocoder geocoder = new Geocoder(this, Locale.KOREA);
+        List<Address> address;
+        String currentLocationAddress;
+        try {
+            if (geocoder != null) {
+                // 세번째 인수는 최대결과값인데 하나만 리턴받도록 설정했다
+                address = geocoder.getFromLocation(lat, lng, 3);
+                // 설정한 데이터로 주소가 리턴된 데이터가 있으면
+                if (address != null && address.size() > 0) {
+                    // 주소
+                    currentLocationAddress = address.get(0).getAddressLine(0).toString();
+
+                    // 전송할 주소 데이터 (위도/경도 포함 편집)
+                    NumberFormat nf = NumberFormat.getInstance();
+                    nf.setMinimumFractionDigits(2);//소수점 아래 최소 자리수
+                    nf.setMaximumFractionDigits(2);//소수점 아래 최대자리수
+                    bf.append(currentLocationAddress).append("\n");
+                    bf.append("lat:").append(nf.format(lat)).append("\n");
+                    bf.append("lng:").append(nf.format(lng));
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bf.toString();
+    }
+
+    public void findlocate(){
         // Acquire a reference to the system Location Manager
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
@@ -39,7 +86,7 @@ public class InsertActivity extends AppCompatActivity {
                 double lat = location.getLatitude();
                 double lng = location.getLongitude();
 
-                logView.setText("latitude: " + lat + ", longitude: " + lng);
+                logView.setText(findAddress(lat,lng));
             }
 
             public void onStatusChanged(String provider, int status, Bundle extras) {
@@ -61,14 +108,15 @@ public class InsertActivity extends AppCompatActivity {
         // 수동으로 위치 구하기
         String locationProvider = LocationManager.GPS_PROVIDER;
         Location lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
-        NumberFormat nf = NumberFormat.getInstance();
-        nf.setMinimumFractionDigits(2);//소수점 아래 최소 자리수
-        nf.setMaximumFractionDigits(2);//소수점 아래 최대자리수
+
         if (lastKnownLocation != null) {
             double lng = lastKnownLocation.getLatitude();
             double lat = lastKnownLocation.getLatitude();
             Log.d("Main", "longtitude=" + lng + ", latitude=" + lat);
-            logView.setText("longtitude=" + nf.format(lng) + ", latitude=" + nf.format(lat));
+            logView.setText(findAddress(lat,lng));
         }
+
     }
+
+
 }
