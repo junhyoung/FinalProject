b9f749506f76a3ec6d0853ed3032fae5775243af
junhyeong <zz7193@naver.com>
2015-12-19 18:26:57 +0900

    커스텀 리스트뷰 객체,레이아웃(12.19)

---
+++ app/src/main/java/github/com/junhyoung/finalproject/ListData.java
@@ -0,0 +1,25 @@
+package github.com.junhyoung.finalproject;
+
+/**
+ * Created by 준형 on 2015-12-19.
+ */
+public class ListData {
+
+    public String mDate;
+    public String mTime;
+    public String mLocate;
+    public String mEvent;
+    public double lat,lng;
+
+    /**
+     * 알파벳 이름으로 정렬
+     */
+    //public static final Comparator<ListData> ALPHA_COMPARATOR = new Comparator<ListData>() {
+    //    private final Collator sCollator = Collator.getInstance();
+
+ //       @Override
+ // public int compare(ListData mListDate_1, ListData mListDate_2) {
+    //        return sCollator.compare(mListDate_1.mTitle, mListDate_2.mTitle);
+      //  }
+   // };
+}
